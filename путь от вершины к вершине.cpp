3ff3aff7f68631264ba0cef6251de0186e43b7d4
Prohorkina <ann2000ann2015@gmail.com>
2017-11-01 22:53:34 +0300

    Update путь от вершины к вершине.cpp

--- путь от вершины к вершине.cpp
+++ путь от вершины к вершине.cpp
@@ -7,12 +7,18 @@ const int MAXN = 1000;
 
 int n,k,m, a[MAXN][MAXN];
 int visited[MAXN];
+int P[MAXN][MAXN];
 
 
 void dfs(int v) {
     for (int i = 0; i < n; i++){
         if ((a[v][i] == 1) && ((visited[i]==0) || (visited[i]>(visited[v]+1)))){
                 visited [i]=visited[v]+1;
+                z=visited[v];
+                for (int k=0; k<z; k++){
+                    P[i][k]=P[v][k];
+                }
+                P[i][z]=i;
                 dfs(i);
         }
     }
@@ -20,21 +26,26 @@ void dfs(int v) {
 
 int main() {
 
-    cin >> n; //количество вершин
+    cin >> n; 
 
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
-            cin >> a[i][j]; // матрица смежности
+            cin >> a[i][j]; 
 
     for (int i = 0; i < n; ++i)
         visited[i] = 0;
 
     cin >> k;
     visited[k]=1;
+    P[k][0]=k;
     dfs(k);
 
     cin >> m;
 
     cout << visited[m]-1 << endl;
+    z=visited[m];
+    for (int i=0; i<z; i++){
+        cout << P[m][i];
+    }
     return 0;
 }
